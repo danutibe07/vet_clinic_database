57c4feee1e52ff370c4673a2d0ae769cb018830c
danutibe07 <danielutibe07@gmail.com>
2023-05-09 15:12:39 +0100

    adding more rows/data to the animals table

--- data.sql
+++ data.sql
@@ -1,4 +1,16 @@
-INSERT INTO animals (name,date_of_birth,neutered,weight_kg,escape_attempt) VALUES ('Agumon','2020-02-03',true,10.23,0);
-INSERT INTO animals (name,date_of_birth,neutered,weight_kg,escape_attempt) VALUES ('Gabumon','2018-11-15',true,8,0);
-INSERT INTO animals (name,date_of_birth,neutered,weight_kg,escape_attempt) VALUES ('Pikachu','2021-01-07',false,15.04,0);
-INSERT INTO animals (name,date_of_birth,neutered,weight_kg,escape_attempt) VALUES ('Devimon','2017-05-12',true,11,0);
+--PROJECT ONE
+INSERT INTO animals (name,date_of_birth,neutered,weight_kg,escape_attempts) VALUES ('Agumon','2020-02-03',true,10.23,0);
+INSERT INTO animals (name,date_of_birth,neutered,weight_kg,escape_attempts) VALUES ('Gabumon','2018-11-15',true,8,0);
+INSERT INTO animals (name,date_of_birth,neutered,weight_kg,escape_attempts) VALUES ('Pikachu','2021-01-07',false,15.04,0);
+INSERT INTO animals (name,date_of_birth,neutered,weight_kg,escape_attempts) VALUES ('Devimon','2017-05-12',true,11,0);
+
+--PROJECT TWO
+INSERT INTO animals (name,date_of_birth,neutered,weight_kg,escape_attempts) VALUES ('Charmander','2020-02-08',false,-11,0);
+INSERT INTO animals (name,date_of_birth,neutered,weight_kg,escape_attempts) VALUES ('Plantmon','2021-11-15',true,-5.7,2);
+INSERT INTO animals (name,date_of_birth,neutered,weight_kg,escape_attempts) VALUES ('Squirtle','1993-04-02',false,-12.13,3);
+INSERT INTO animals (name,date_of_birth,neutered,weight_kg,escape_attempts) VALUES ('Angemon','2005-06-12',true,-45,1);
+INSERT INTO animals (name,date_of_birth,neutered,weight_kg,escape_attempts) VALUES ('Boarmon','2018-06-07',true,20.4,7);
+INSERT INTO animals (name,date_of_birth,neutered,weight_kg,escape_attempts) VALUES ('Blossom','1998-10-13',false,17,3);
+INSERT INTO animals (name,date_of_birth,neutered,weight_kg,escape_attempts) VALUES ('Blossom','2022-05-14',true,22,4);
+
+
